91948372a591cd9ef67d026fa5bd0926579ca0eb
famna <famavolatt@gmail.com>
2025-03-26 16:01:10 +0200

    More funct

--- test_main.py
+++ test_main.py
@@ -1,57 +1,68 @@
+import pytest
+import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
-class BaseTest:
-    def setup_method(self):
-        print("Налаштовую драйвер...")
-        
-        # Налаштування параметрів Chrome
-        options = Options()
-        # Вимкніть headless для перевірки
-        # options.add_argument("--headless")  # Вимкнення headless режиму для відображення браузера
-        options.add_argument("--disable-gpu")  # Вимкнення GPU для кращої роботи
-        options.add_argument("--window-size=1920,1080")  # Розмір вікна
+# Налаштування логування
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-        # Встановлення драйвера Chrome
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        print("Драйвер запущено!")
-        
-        self.driver.implicitly_wait(10)  # Очікування елементів на сторінці
-        print("Браузер відкрито!")
+@pytest.fixture(scope="class")
+def setup_driver():
+    logging.info("Налаштовую драйвер...")
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.implicitly_wait(10)
+    logging.info("Браузер відкрито!")
+    yield driver
+    logging.info("Закриваю драйвер...")
+    driver.quit()
+    logging.info("Драйвер закрито.")
 
-    def teardown_method(self):
-        print("Закриваю драйвер...")
-        self.driver.quit()
-        print("Драйвер закрито.")
+class TestLogin:
+    def test_login_success(self, setup_driver):
+        logging.info("Відкриваю сторінку логіну...")
+        setup_driver.get("https://the-internet.herokuapp.com/login")
+        setup_driver.find_element(By.ID, "username").send_keys("tomsmith")
+        setup_driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+        setup_driver.find_element(By.CSS_SELECTOR, "button.radius").click()
+        assert "You logged into a secure area!" in setup_driver.find_element(By.ID, "flash").text
+        logging.info("Успішний логін підтверджено!")
 
-class LoginTest(BaseTest):
-    def test_login(self):
-        print("Відкриваю сторінку...")
-        self.driver.get("https://the-internet.herokuapp.com/login")
-        
-        # Знаходимо елементи на сторінці
-        username_input = self.driver.find_element(By.ID, "username")
-        password_input = self.driver.find_element(By.ID, "password")
-        login_button = self.driver.find_element(By.CSS_SELECTOR, "button.radius")
-        
-        # Вводимо дані та натискаємо кнопку
-        username_input.send_keys("tomsmith")
-        password_input.send_keys("SuperSecretPassword!")
-        login_button.click()
-        
-        # Перевіряємо, чи вдалося залогінитись
-        \
-        success_message = self.driver.find_element(By.ID, "flash")
-        assert "You logged into a secure area!" in success_message.text
-        
-        print("Тест завершено!")
+    def test_login_failure(self, setup_driver):
+        logging.info("Перевірка некоректного логіну...")
+        setup_driver.get("https://the-internet.herokuapp.com/login")
+        setup_driver.find_element(By.ID, "username").send_keys("wronguser")
+        setup_driver.find_element(By.ID, "password").send_keys("wrongpassword")
+        setup_driver.find_element(By.CSS_SELECTOR, "button.radius").click()
+        assert "Your username is invalid!" in setup_driver.find_element(By.ID, "flash").text
+        logging.info("Перевірка некоректного логіну пройдена!")
+
+class TestCheckbox:
+    def test_checkboxes(self, setup_driver):
+        logging.info("Перевірка чекбоксів...")
+        setup_driver.get("https://the-internet.herokuapp.com/checkboxes")
+        checkboxes = setup_driver.find_elements(By.CSS_SELECTOR, "input[type='checkbox']")
+        for checkbox in checkboxes:
+            if not checkbox.is_selected():
+                checkbox.click()
+        assert all(checkbox.is_selected() for checkbox in checkboxes)
+        logging.info("Усі чекбокси вибрані!")
+
+class TestAlert:
+    def test_alerts(self, setup_driver):
+        logging.info("Перевірка спливаючих вікон...")
+        setup_driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+        setup_driver.find_element(By.XPATH, "//button[text()='Click for JS Alert']").click()
+        alert = setup_driver.switch_to.alert
+        assert "I am a JS Alert" in alert.text
+        alert.accept()
+        logging.info("Спливаюче вікно успішно закрито!")
 
 if __name__ == "__main__":
-    test = LoginTest()
-    test.setup_method()
-    test.test_login()
-    test.teardown_method()
+    pytest.main(["-v", "--html=report.html"])
+
